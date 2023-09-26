da0e6c7f2874bc2771b495feccbc23a4c35b61cc
Fahad Rajput <fahadameenrajput@gmail.com>
2023-09-26 18:14:30 +0500

    go

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,11 +11,11 @@ const Home = () => {
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-md-8 mt-5">
-                            <h1 className="display-4 fw-bolder mb-4 text-center text-white">Something</h1>
+                            <h1 className="display-4 fw-bolder mb-4 text-center text-white">Welcome</h1>
                             <p className="lead text-center fs-4 mb-5 text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit obcaecati aut molestiae porro reiciendis consectetur maiores atque necessitatibus blanditiis provident.</p>
                             <div className="buttons d-flex justify-content-center">
-                                <NavLink to="/contact" className="btn btn-light me-4 rounded-pill px-4 py-2">Something</NavLink>
-                                <NavLink to="/service" className="btn btn-outline-light rounded-pill px-4 py-2">Something</NavLink>
+                                <NavLink to="/contact" className="btn btn-light me-4 rounded-pill px-4 py-2">Contact Us</NavLink>
+                                <NavLink to="/service" className="btn btn-outline-light rounded-pill px-4 py-2">Services</NavLink>
                             </div>
                         </div>
                     </div>
